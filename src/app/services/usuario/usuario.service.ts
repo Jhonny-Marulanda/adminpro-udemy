6cd3f27d9cc9a6d9cb2e99d0bafab8e991c15f4b
unknown <jalejandro.marulanda@gmail.com>
2020-01-17 13:58:33 -0500

    Fin de la sección 17

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -3,17 +3,25 @@ import { Usuario } from '../../models/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../config/config';
 
+
+//import { Observable } from 'rxjs/Observable';
+//import 'rxjs/add/operator/map';
+//import 'rxjs/add/operator/catch';
+
 import { Router } from '@angular/router';
 import { SubirArchivoService } from '../subir-archivo/subir-archivo.service';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 
 import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+//import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable()
 export class UsuarioService {
 
   usuario: Usuario;
   token: string;
+  menu: any[] = [];
 
   constructor(
     public http: HttpClient,
@@ -32,29 +40,35 @@ export class UsuarioService {
     if ( localStorage.getItem('token')) {
       this.token = localStorage.getItem('token');
       this.usuario = JSON.parse( localStorage.getItem('usuario') );
+      this.menu = JSON.parse( localStorage.getItem('menu') );
     } else {
       this.token = '';
       this.usuario = null;
+      this.menu = [];
     }
 
   }
 
-  guardarStorage( id: string, token: string, usuario: Usuario ) {
+  guardarStorage( id: string, token: string, usuario: Usuario, menu: any ) {
 
     localStorage.setItem('id', id );
     localStorage.setItem('token', token );
     localStorage.setItem('usuario', JSON.stringify(usuario) );
+    localStorage.setItem('menu', JSON.stringify(menu) );
 
     this.usuario = usuario;
     this.token = token;
+    this.menu = menu;
   }
 
   logout() {
     this.usuario = null;
     this.token = '';
+    this.menu = [];
 
     localStorage.removeItem('token');
     localStorage.removeItem('usuario');
+    localStorage.removeItem('menu');
 
     this.router.navigate(['/login']);
   }
@@ -65,7 +79,7 @@ export class UsuarioService {
 
     return this.http.post( url, { token } ).pipe(
                 map( (resp: any) => {
-                  this.guardarStorage( resp.id, resp.token, resp.usuario );
+                  this.guardarStorage( resp.id, resp.token, resp.usuario, resp.menu );
                   return true;
                 }));
 
@@ -83,10 +97,21 @@ export class UsuarioService {
     let url = URL_SERVICIOS + '/login';
     return this.http.post( url, usuario ).pipe(
                 map( (resp: any) => {
-
-                  this.guardarStorage( resp.id, resp.token, resp.usuario );
-
+                  this.guardarStorage( resp.id, resp.token, resp.usuario, resp.menu );
                   return true;
+                }),
+                catchError( err => {
+                //.catch( err => {
+
+                  //swal( 'Error en el login', err.error.mensaje, 'error' );
+                  Swal.fire({
+                    title: 'Error en el login',
+                    text: err.error.mensaje,
+                    //type: 'info',
+                    allowOutsideClick: false
+                  });
+
+                  return Observable.throw( err );
                 }));
 
   }
@@ -106,8 +131,20 @@ export class UsuarioService {
                   //type: 'info',
                   allowOutsideClick: false
                 });
-                
+
                 return resp.usuario;
+              }),
+              catchError (err => {
+              //.catch( err => {
+                //swal( err.error.mensaje, err.error.errors.message, 'error' );
+                Swal.fire({
+                  title: err.error.mensaje,
+                  text: err.error.errors.message,
+                  //type: 'info',
+                  allowOutsideClick: false
+                });
+
+                return Observable.throw( err );
               }));
   }
 
@@ -121,18 +158,30 @@ export class UsuarioService {
 
                   if ( usuario._id === this.usuario._id ) {
                     let usuarioDB: Usuario = resp.usuario;
-                    this.guardarStorage( usuarioDB._id, this.token, usuarioDB );
+                    this.guardarStorage( usuarioDB._id, this.token, usuarioDB, this.menu );
                   }
 
                   //swal('Usuario actualizado', usuario.nombre, 'success' );
                   Swal.fire({
-                    title: 'Usuario Actualizado',
+                    title: 'Usuario actualizado',
                     text: usuario.nombre,
                     //type: 'info',
                     allowOutsideClick: false
                   });
 
                   return true;
+                }),
+                catchError( err => {
+                //.catch( err => {
+                  //swal( err.error.mensaje, err.error.errors.message, 'error' );
+                  Swal.fire({
+                    title: err.error.mensaje,
+                    text: err.error.errors.mensaje,
+                    //type: 'info',
+                    allowOutsideClick: false
+                  });
+
+                  return Observable.throw( err );
                 }));
 
   }
@@ -150,7 +199,8 @@ export class UsuarioService {
               //type: 'info',
               allowOutsideClick: false
             });
-            this.guardarStorage( id, this.token, this.usuario );
+
+            this.guardarStorage( id, this.token, this.usuario, this.menu );
 
           })
           .catch( resp => {
@@ -183,11 +233,12 @@ export class UsuarioService {
                 map( resp => {
                   //swal('Usuario borrado', 'El usuario a sido eliminado correctamente', 'success');
                   Swal.fire({
-                    title: 'Usuario borrado',
+                    title: 'Usario borrado',
                     text: 'El usuario a sido eliminado correctamente',
                     //type: 'info',
                     allowOutsideClick: false
                   });
+
                   return true;
                 }));
 
